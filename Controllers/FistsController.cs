8039ec15efc5ea9051e7dcef5a82f061d8aed2d0
Nick Allmaker <nicka@Nicks-MBP.agapered.org>
2019-05-01 15:36:00 -0500

    Moar Fists

--- Controllers/FistsController.cs
+++ Controllers/FistsController.cs
@@ -19,6 +19,8 @@ namespace fists.Controllers
             ("Ebony", "Ivory"),
             ("Left", "Left Jr."),
             ("John", "Frerichs"),
+            ("Tinder", "Grindr"),
+            ("Smash U", "Burn U Up"),
         };
 
         // GET api/values
