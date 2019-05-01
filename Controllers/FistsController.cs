9e02ff416cb5f18149a0fb56ca051cfa947a0310
Nick Allmaker <nicka@Nicks-MBP.agapered.org>
2019-05-01 14:38:16 -0500

    say hello

---
+++ Controllers/FistsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fists.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FistsController : ControllerBase
+    {
+        static Random rng = new Random();
+        static (string, string)[] fists = new (string, string)[] {
+            ("Jack Johnson", "Tom O'Leary"),
+            ("LEFT", "RGHT"),
+            ("Westside", "Eastside"),
+            ("Mortar", "Pestle"),
+            ("Ebony", "Ivory"),
+        };
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var myFists = fists[rng.Next(fists.Length)];
+            return string.Format("Say hello to my friends {0} and {1}", myFists.Item1, myFists.Item2);
+        }
+   }
+}
